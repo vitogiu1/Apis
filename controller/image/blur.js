ac938c7581f3554e2988a74a8ef5433f1ccd720f
CloudyyUw <69282305+CloudyyUw@users.noreply.github.com>
2021-02-10 22:46:12 -0300

    Create blur.js

---
+++ controller/image/blur.js
@@ -0,0 +1,16 @@
+const Jimp = require("jimp")
+
+module.exports = async (req, res) => {
+  const { image, amount } = req.query;
+  const blurA = Number(amount) || 20
+  const Image = await Jimp.read(image);
+  if (!Image) {
+    return res.status(400).json({
+      status: "400: Bad Request",
+      message: "You need to provide a valid image."
+    })
+  };
+  Image.blur(blurA);
+  const buffer = await Image.getBufferAsync("image/png");
+  return res.end(buffer)
+}
