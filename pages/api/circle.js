9bfd3460fd6eae16bab7a89a9b68a2d8d3ee80ab
CloudyyUw <69282305+CloudyyUw@users.noreply.github.com>
2021-02-12 09:29:11 -0300

    Update circle.js

--- pages/api/circle.js
+++ pages/api/circle.js
@@ -1,18 +1,24 @@
 const Jimp = require('jimp');
 
 export default async function handler(req, res) {
-    res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
-    const { image, size } = req.query;
-    const Size = Number(size) || 1024
-    const Image = await Jimp.read(image);
-    if (!Image) {
-        return res.status(400).json({
-            status: "400: Bad Request",
-            message: "You need to provide a valid image."
-        })
-    };
-    Image.resize(Size, Size)
-    Image.circle()
-    const buffer = await Image.getBufferAsync("image/png");
-    return res.end(buffer)
+  res.setHeader("Cache-Control", "s-maxage=10, stale-while-revalidate");
+  const { image, size } = req.query;
+  const Size = Number(size);
+  if (isNaN(Size) || Size < 1) {
+    return res.status(400).json({
+      status: "400: Bad Request",
+      message: "You need to inform the size of the image."
+    })
+  }
+  const Image = await Jimp.read(image);
+  if (!Image) {
+    return res.status(400).json({
+      status: "400: Bad Request",
+      message: "You need to provide a valid image."
+    })
+  };
+  Image.resize(Size, Size)
+  Image.circle()
+  const buffer = await Image.getBufferAsync("image/png");
+  return res.end(buffer)
 }
