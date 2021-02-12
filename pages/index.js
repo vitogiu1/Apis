8f4a53b986b1f0cc2e80e5349c756784c3436754
CloudyyUw <69282305+CloudyyUw@users.noreply.github.com>
2021-02-12 09:17:48 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,9 @@
-export default function home() {
+export default function Home() {
   return (
     <center>
       <p>
         Read the docs on github <a href='https://github.com/CloudyyUw/Apis'>here</a>
       </p>
     </center>
-    );
+    )
 }
